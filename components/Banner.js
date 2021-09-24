c4deafe29ff0f7c01aed00413ae715e72e867096
Tim Grossi <timgrossi@users.noreply.github.com>
2021-09-23 17:59:19 -0700

    Added InfoCards implementing TailwindCSS

--- components/Banner.js
+++ components/Banner.js
@@ -11,7 +11,7 @@ function Banner() {
             />
             <div className="absolute top-1/2 w-full text-center">
                 <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
-                <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition durration-150">Im Flexible</button>
+                <button className="text-green-400 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition durration-150">Im Flexible</button>
             </div>
         </div>
     )
